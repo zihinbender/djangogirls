c35d105647b0eb2d18e021c3bf84048343491322
zihinbender <73230187+zihinbender@users.noreply.github.com>
2021-11-07 22:08:52 +0300

    static admn

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/admin'
-STATIC_ROOT = os.path.join(BASE_DIR, '/home/mindbender/mindbender.pythonanywhere.com/mysite/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/admin')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
