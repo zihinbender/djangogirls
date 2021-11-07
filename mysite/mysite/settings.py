c8b5ae3da3d8c4b022816828a0090087b56b1470
zihinbender <73230187+zihinbender@users.noreply.github.com>
2021-11-07 22:06:50 +0300

    static admin

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/home/mindbender/mindbender.pythonanywhere.com/mysite/static/'
+STATIC_URL = '/static/admin'
 STATIC_ROOT = os.path.join(BASE_DIR, '/home/mindbender/mindbender.pythonanywhere.com/mysite/static/')
 
 # Default primary key field type
