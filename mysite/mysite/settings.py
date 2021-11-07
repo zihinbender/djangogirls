b90d1810e4d582dd1ad6b8a6494b3b14c2f91ff0
zihinbender <budakaziz@gmail.com>
2021-11-07 16:46:22 +0300

    added css

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ca*y7o!1s#c8w2#basl$yptgf+z(e2n+qb_(k&c*jmm26%h%&%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mindbender.pythonanywhere.com']
 
 
 # Application definition
