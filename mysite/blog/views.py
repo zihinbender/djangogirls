5cd997b3c6ef98d8579060584fc8d799fe6eedc7
zihinbender <73230187+zihinbender@users.noreply.github.com>
2021-11-08 14:18:52 +0300

    Update views.py

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -17,7 +17,16 @@ def post_detail(request, pk):
     })
 
 def post_new(request):
-    form=PostForm()
+    if request.method == POST:
+        form=PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form=PostForm()
     return render(request, 'blog/post_edit.html', {
         'form': form
     })
